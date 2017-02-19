75b4d96a8d8a627ea23b50dd49bec74c0f3c8eaa
Alexandru Stan <alexandrust@ceragon.com>
2017-02-19 20:14:17 +0200

    stanila la platane

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
-# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "anthony"
-client_key               "#{current_dir}/anthony.pem"
-chef_server_url          "https://anthony2.mylabserver.com/organizations/linuxacademy"
+node_name                "unlessme"
+client_key               "#{current_dir}/unlessme.pem"
+chef_server_url          "https://api.chef.io/organizations/stanila"
 cookbook_path            ["#{current_dir}/../cookbooks"]
